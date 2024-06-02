41331f4c44b31369998008b5afd2d8574e9956ec
Adnan <adnanali23189@gmail.com>
2024-06-02 23:09:13 +0530

    container commit

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "./ui/tracing-beam";
-import About from '../assets/about_image.jpg';
-import Canon_IR_ADV from '../assets/Canon IR ADV 4225.jpg';
 import { BgBeams } from "./BgBeams";
 
 
